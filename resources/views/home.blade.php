f82112b37e3ec130b77343f7b5e807ca654e000f
Herdi Hidayat <herdi.kom@gmail.com>
2021-03-27 19:43:26 +0700

    TestBootcampDumbwaysid

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('template.master')
+@section('title', 'Dashboard')
+@section('content')
+<div class="container">
+          <div class="row">
+               <div class="col-md-12">
+                    <div class="card">
+                    <div class="card-header">
+                         <h3>Test Technical Online Bootcamp DumbWays.id
+FULLSTACK DEVELOPER Sabtu 27 Maret 2021</h3>
+</div>
+                    </div>
+               </div>
+          </div>
+     </div>
+@endsection
